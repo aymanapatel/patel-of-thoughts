c75ee1686b72a1a56ccccdd5f1f89257c14480f3
Ayman Patel <ayman.patel97@outlook.com>
2026-02-03 22:07:50 +0000

    feat: Add cover in blogs

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,6 +10,7 @@ const posts = defineCollection({
         tags: z.array(z.string()).default([]),
         draft: z.boolean().default(false),
         heroImage: z.string().optional(),
+        cover: z.string().optional(),
     }),
 });
 
